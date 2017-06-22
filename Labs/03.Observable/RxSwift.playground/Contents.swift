aee6acda8dbe0e5b15b52d07a595efbc61a76d6c
Olarn U <olarn.u@gmail.com>
2017-06-22 12:23:09 +0700

    Add more slides and example codes in 03.

--- Labs/03.Observable/RxSwift.playground/Contents.swift
+++ Labs/03.Observable/RxSwift.playground/Contents.swift
@@ -4,6 +4,49 @@ XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
 
 import RxSwift
 
-example(of: "Hello, World") { 
-    print("Hello, World!")
+example(of: "just") {
+
+    let one = 1
+
+    let observable: Observable<Int> = Observable<Int>.just(one)
+    observable.subscribe { event in
+        print(event)
+    }
 }
+
+example(of: "Of") {
+
+    let one = 1
+    let two = 2
+    let three = 3
+
+    let observable = Observable.of(one, two, three)
+    observable.subscribe { event in
+        print(event)
+    }
+}
+
+example(of: "Of Array") {
+
+    let one = 1
+    let two = 2
+    let three = 3
+
+    let observable = Observable.of([one, two, three])
+    observable.subscribe { event in
+        print(event)
+    }
+}
+
+example(of: "From") {
+
+    let one = 1
+    let two = 2
+    let three = 3
+
+    let observable = Observable.from([one, two, three])
+    observable.subscribe { event in
+        print(event)
+    }
+}
+
