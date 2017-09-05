87bba7a385e08d0c12e55976387886d5dc316018
Olarn U <olarn.u@gmail.com>
2017-09-05 09:31:09 +0700

    Add BehaviorSubject contents and labs.

--- Labs/04.Subjects/BehaviorSubject.playground/Contents.swift
+++ Labs/04.Subjects/BehaviorSubject.playground/Contents.swift
@@ -1,5 +1,20 @@
 import RxSwift
 
 example(of: "Behavior Subject") { 
-    
+    let bag = DisposeBag()
+
+    let subject = BehaviorSubject(value: "Initial Value")
+    let subscriber1 = subject.subscribe {
+        printEvent(label: "1)", event: $0)
+    }
+    subscriber1.disposed(by: bag)
+
+    subject.onNext("Second Value")
+
+    let subscriber2 = subject.subscribe {
+        printEvent(label: "2)", event: $0)
+    }
+    subscriber2.disposed(by: bag)
+
+    subject.onError(MyError.anError)
 }
