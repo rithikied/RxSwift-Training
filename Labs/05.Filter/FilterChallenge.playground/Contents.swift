725dab00263bab58ba5a4968d5b6c84bf39a54b1
Olarn U <olarn.u@gmail.com>
2017-09-08 10:15:10 +0700

    change filenames and add 06 slide.

---
+++ Labs/05.Filter/FilterChallenge.playground/Contents.swift
@@ -0,0 +1,57 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import RxSwift
+
+let contacts = [
+    "603-555-1212": "Florent",
+    "212-555-1212": "Junior",
+    "408-555-1212": "Marin",
+    "617-555-1212": "Scott"
+]
+
+func phoneNumber(from inputs: [Int]) -> String {
+    var phone = inputs.map(String.init).joined()
+    phone.insert("-", at: phone.index(
+        phone.startIndex,
+        offsetBy: 3)
+    )
+    phone.insert("-", at: phone.index(
+        phone.startIndex,
+        offsetBy: 7)
+    )
+    return phone
+}
+
+challenge(of: "Phone Filter") {
+
+    let bag = DisposeBag()
+    let input = PublishSubject<Int>()
+
+    input
+        .skipWhile { $0 == 0 || $0 > 9}
+        .take(10)
+        .toArray()
+        .subscribe(onNext: {
+            let phone = phoneNumber(from: $0)
+            if let contact = contacts[phone] {
+                print("Dialing \(contact) (\(phone))...")
+            } else {
+                print("Contact not found")
+            }
+        }).disposed(by: bag)
+
+    input.onNext(0)
+    input.onNext(603)
+    input.onNext(2)
+    input.onNext(1)
+    input.onNext(2)
+
+    "5551212".characters.forEach({
+        if let number = (Int("\($0)")) {
+            input.onNext(number)
+        }
+    })
+
+    input.onNext(9)
+}
--- Labs/05.Operators/FilterChallenge.playground/Sources/SupportCode.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-public func challenge(of description: String, action: () -> Void) {
-    print("\n--- Challenge of:", description, "---\n")
-    action()
-}
