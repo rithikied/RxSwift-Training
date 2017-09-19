765335f4f2f97e917355e7c725a9e53c4ab6c5b6
Olarn U <olarn.u@gmail.com>
2017-09-19 10:04:04 +0700

    Add 08.TimeBased

---
+++ Labs/08.TimeBased/RxSwift.playground/Pages/timeout.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: Please build the scheme 'RxSwiftPlayground' first
+import UIKit
+import RxSwift
+import RxCocoa
+
+// Support code -- DO NOT REMOVE
+class TimelineView<E>: TimelineViewBase, ObserverType where E: CustomStringConvertible {
+
+    static func make() -> TimelineView<E> {
+        return TimelineView(width: 400, height: 100)
+    }
+
+    public func on(_ event: Event<E>) {
+        switch event {
+        case .next(let value):
+            add(.Next(String(describing: value)))
+        case .completed:
+            add(.Completed())
+        case .error(_):
+            add(.Error())
+        }
+    }
+}
