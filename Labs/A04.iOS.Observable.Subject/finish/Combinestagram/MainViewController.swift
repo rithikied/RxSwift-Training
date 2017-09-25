c01d7522c0a8468c48412fdd26bb30097ec19526
Olarn U <olarn.u@gmail.com>
2017-09-25 11:44:32 +0700

    Complete A04.iOS.Observable.Subject.

--- Labs/A04.iOS.Observable.Subject/finish/Combinestagram/MainViewController.swift
+++ Labs/A04.iOS.Observable.Subject/finish/Combinestagram/MainViewController.swift
@@ -88,12 +88,21 @@ class MainViewController: UIViewController {
     }
     
     @IBAction func actionSave() {
-        
-    }
-    
-    func showMessage(_ title: String, description: String? = nil) {
-        let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { [weak self] _ in self?.dismiss(animated: true, completion: nil)}))
-        present(alert, animated: true, completion: nil)
+        guard let image = imagePreview.image else { return }
+        PhotoWriter.save(image)
+            .subscribe(onError: { [weak self] error in
+                if let view = self {
+                    view.showMessage(title: "Error", description: error.localizedDescription)
+                        .subscribe().disposed(by: view.bag)
+                }
+                }, onCompleted: { [weak self] in
+                    if let view = self {
+                        view.showMessage(title: "Save", description: "")
+                            .subscribe()
+                            .disposed(by: view.bag)
+                        view.actionClear()
+                    }
+            })
+            .disposed(by: bag)
     }
 }
