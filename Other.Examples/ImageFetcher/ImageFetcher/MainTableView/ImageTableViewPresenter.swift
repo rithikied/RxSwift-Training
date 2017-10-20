122e023928fecf599af1d2f4eadeeeee1f2dd27b
Olarn U <olarn.u@gmail.com>
2017-10-20 10:03:18 +0700

    Remove unused text.

---
+++ Other.Examples/ImageFetcher/ImageFetcher/MainTableView/ImageTableViewPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  ImageTableViewPresenter.swift
+//  ImageFetcher
+//
+//  Created by Olarn U. on 19/10/2560 BE.
+//  Copyright © 2560 Olarn U. All rights reserved.
+//
+
+import Foundation
+
+class ImageTableViewPresenter {
+    
+}
---
+++ Other.Examples/ImageFetcher/ImageFetcher/MainTableView/MainTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MainTableViewController.swift
+//  ImageFetcher
+//
+//  Created by Olarn U. on 19/10/2560 BE.
+//  Copyright © 2560 Olarn U. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class MainTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
