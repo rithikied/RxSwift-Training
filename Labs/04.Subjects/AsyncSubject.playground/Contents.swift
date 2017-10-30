6cdfefa64a22c94a12d7f3cf2d7a4c661ae8eeda
Olarn U <olarn.u@gmail.com>
2017-10-30 11:14:18 +0700

    Add AsyncSubject to 04.Subject lab.

---
+++ Labs/04.Subjects/AsyncSubject.playground/Contents.swift
@@ -0,0 +1,18 @@
+import RxSwift
+
+example(of: "Async Subject") {
+    let bag = DisposeBag()
+    
+    let subject = AsyncSubject<String>()
+    
+    subject.onNext("1")
+    
+    subject.subscribe {
+        print($0)
+    }.disposed(by: bag)
+    
+    subject.onNext("2")
+    subject.onNext("3")
+
+    subject.onCompleted()
+}
